36dd9de9938c3b6e366d5711f3982b5e8a68fc73
satindersingh <satindersingh472@gmail.com>
2022-09-20 19:10:58 -0600

    finished

--- fundamentals.py
+++ fundamentals.py
@@ -1,6 +1,17 @@
-number=5
+number= 110
 string='hello! world!!'
-boolean=True
+boolean= True
 print(number)
 print(string)
 print(boolean)
+if(number>10):
+    print('That is larger than 10')
+else:
+    print('That is NOT larger than 10')
+
+if(number <= -1 and boolean == True):
+    print('Negative and true')
+elif(number>=0 and boolean == False):
+    print('Positive and False')
+elif(number>100 or boolean == True):
+    print('Large and True')
