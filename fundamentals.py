726fdfec204cb415cdb71e3ca32e9aed7c0d6472
satindersingh <satindersingh472@gmail.com>
2022-09-20 19:31:08 -0600

    updated

--- fundamentals.py
+++ fundamentals.py
@@ -1,6 +1,13 @@
 number= 50
 string='hello! world!!'
 boolean= True
+array_string = ['Javascript','SQL','Mariadb','Python']
+array_num = [22,44,66,88]
+for word in array_string:
+    print(word)
+for num in array_num:
+    print( 'Look at this number',num)
+
 print(number)
 print(string)
 print(boolean)
@@ -17,3 +24,4 @@ elif(number>100 or boolean == True):
     print('Large and True')
 else:
     print("I don't know")
+
